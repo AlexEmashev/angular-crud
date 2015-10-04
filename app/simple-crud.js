24ae39ba91c08930b0fde89b7c91b216a6d5bb91
Alexander Emashev <alex.emashev@gmail.com>
2015-10-04 21:37:42 +0300

    Add create new user method.

--- app/simple-crud.js
+++ app/simple-crud.js
@@ -2,9 +2,25 @@
 var app = angular.module('simpleCrud',[]);
 
 // Create controller 'crudCtrl' to manage the data on the page
-app.controller('crudCtrl', function($scope, $http){
+app.controller('crudCtrl', function($scope, $http){  
   // Test of reading data from remote service
   $http.get("http://jsonplaceholder.typicode.com/users")
     .success(function(resoponse) { $scope.users = resoponse; })
     .error(function(response) { console.log("Request error: %s", response); });
+    
+  // Method create new record
+  $scope.create = function(){
+    $http.post("http://jsonplaceholder.typicode.com/users", $scope.user)
+      .success(function(response) { 
+        console.log(response);
+        $scope.users.push(response); 
+      })
+      .error(function(response) {console.log("error " + response)});
+  };
+  
+  // Method for creating a new user
+  $scope.new = function(){
+    var newUserId =  $scope.users.count;
+    $scope.user = {id: newUserId, name: "", email: ""};
+  };
 });
