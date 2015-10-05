2211b67e02843bf8447976576bce319160c9a2e6
Alexander Emashev <a.yemashev@obespechenie-vniief.ru>
2015-10-05 14:03:31 +0300

    Fixed formatting in module script.

--- app/simple-crud.js
+++ app/simple-crud.js
@@ -1,26 +1,26 @@
 // Create an angular app module with name 'simpleCrud'
-var app = angular.module('simpleCrud',[]);
+var app = angular.module('simpleCrud', []);
 
 // Create controller 'crudCtrl' to manage the data on the page
-app.controller('crudCtrl', function($scope, $http){  
+app.controller('crudCtrl', function ($scope, $http) {  
   // Test of reading data from remote service
   $http.get("http://jsonplaceholder.typicode.com/users")
-    .success(function(resoponse) { $scope.users = resoponse; })
-    .error(function(response) { console.log("Request error: %s", response); });
+    .success(function (resoponse) { $scope.users = resoponse; })
+    .error(function (response) { console.log("Request error: %s", response); });
   
   // Set selected row in table
-  $scope.selectRow = function(id){
+  $scope.selectRow = function(id) {
     $scope.selectedId = id;
-  }
+  };
     
   // Method create new record
-  $scope.create = function(){
+  $scope.create = function (){
     $http.post("http://jsonplaceholder.typicode.com/users", $scope.user)
       .success(function(response) {
         console.log(response);
-        $scope.users.push(response); 
+        $scope.users.push(response);
       })
-      .error(function(response) {console.log("error " + response)});
+      .error(function (response) {console.log("error " + response);});
   };
   
   // Method for creating a new user
@@ -30,7 +30,7 @@ app.controller('crudCtrl', function($scope, $http){
   };
   
   // Method for editing a user
-  $scope.edit = function(id){
+  $scope.edit = function(id) {
 //    var newUserId =  $scope.users.count;
 //    $scope.user = {id: newUserId, name: "", email: ""};
   };
