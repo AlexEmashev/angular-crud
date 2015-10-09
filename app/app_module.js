4e0c24adf3bdc5d6245f23d450cb78489d09abed
Alexander Emashev <a.yemashev@obespechenie-vniief.ru>
2015-10-09 16:06:11 +0300

    Replace bootstrap validation with angular-validation plugin

--- app/app_module.js
+++ app/app_module.js
@@ -2,6 +2,17 @@
 // Using bootstrap component to organize the UI
 // ngAnimate - used by bootsrap for modal fade-in\fade-out effects
 // bootstrap.showErrors - is a custom directive for form validation
+// var app is the reference that other parts of the application use.
 var app = angular.module('simpleCrud', ['ui.bootstrap', 'ngAnimate', 'ui.bootstrap.showErrors',
                                        'ui.select', 'ngSanitize']);
-// var app is the reference that other parts of the application use.
+
+// Include validation languages
+app.config(function($translateProvider){
+  $translateProvider.useStaticFilesLoader({
+    prefix: 'js/angular-validation/locales',
+    suffix: '.json'
+  });
+  
+  // Define translation map to use on startup
+  $translateProvider.preferredLanguage('en');
+})
